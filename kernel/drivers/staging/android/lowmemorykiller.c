348727c3c563b61c5a78e589fe0c8696b97a300d
hafidzduddin <hafidzduddin@gmail.com>
2012-11-29 05:34:59 +0700

    LMK Test

--- kernel/drivers/staging/android/lowmemorykiller.c
+++ kernel/drivers/staging/android/lowmemorykiller.c
@@ -41,18 +41,22 @@
 static uint32_t lowmem_debug_level = 2;
 static int lowmem_adj[6] = {
 	0,
-	1,
+	3,
 	6,
+	10,
 	12,
+	15,
 };
-static int lowmem_adj_size = 4;
+static int lowmem_adj_size = 6;
 static size_t lowmem_minfree[6] = {
-	3 * 512,	/* 6MB */
-	2 * 1024,	/* 8MB */
-	4 * 1024,	/* 16MB */
-	16 * 1024,	/* 64MB */
+	2048,
+	3584,
+	20992,
+	23808,
+	26624,
+	29440,
 };
-static int lowmem_minfree_size = 4;
+static int lowmem_minfree_size = 6;
 
 static struct task_struct *lowmem_deathpending;
 static unsigned long lowmem_deathpending_timeout;
