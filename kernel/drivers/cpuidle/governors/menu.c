0ce71dda15f8e303a5b3efd4a3e3edade8ce43a7
hafidzduddin <hafidzduddin@gmail.com>
2012-11-29 05:53:12 +0700

    Reach deeper C idle states faster. From imoseyon.

--- kernel/drivers/cpuidle/governors/menu.c
+++ kernel/drivers/cpuidle/governors/menu.c
@@ -182,7 +182,7 @@ static inline int performance_multiplier(void)
 	mult += 2 * get_loadavg();
 
 	/* for IO wait tasks (per cpu!) we add 5x each */
-	mult += 10 * nr_iowait_cpu(smp_processor_id());
+	//mult += 10 * nr_iowait_cpu(smp_processor_id());
 
 	if (tune_multiplier != 1024)
 		mult = (tune_multiplier * mult) / 1024;
